435f9330d59493715e8a547724dc581083f16ceb
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-06-13 18:51:10 +0100

    ok

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, View, Text} from 'react-native';
+//import tailwind from 'tailwind-rn';
+import {create} from 'tailwind-rn';
+import styles from './styles.json';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const {tailwind, getColor} = create(styles);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+tailwind('text-blue-500 text-opacity-50');
+
+const App = () => (
+	<SafeAreaView style={tailwind('h-full')}>
+		<View style={tailwind('pt-12 items-center')}>
+			<View style={tailwind('bg-primary px-3 py-1 rounded-full')}>
+				<Text style={tailwind('text-blue-800 font-semibold')}>
+					Hello Tailwind
+				</Text>
+			</View>
+		</View>
+	</SafeAreaView>
+);
+
+export default App;
