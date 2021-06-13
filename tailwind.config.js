a8a221060f3c452a6dc984b73ca9440815d96842
Allani Ahmed <67459843+ahmedallani@users.noreply.github.com>
2021-06-13 19:47:01 +0100

    custom tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#007bff",
+        success: "#28a745",
+        warning: "#ffc107",
+        danger: "#dc3545",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
